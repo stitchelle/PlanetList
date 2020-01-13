32dd39441b3d8e7481d636302b9badc06830492c
Michelle Johnson <bob1234girl@gmail.com>
2020-01-13 14:50:15 -0600

    extend()

--- planets.py
+++ planets.py
@@ -3,3 +3,11 @@ planet_list = ["Mercury", "Mars"]
 planet_list.append("Jupiter")
 planet_list.append("Saturn")
 print (planet_list)
+
+# another list of planets
+planet_list1 = ["Uranus", "Neptune"]
+
+planet_list.extend(planet_list1)
+
+# Extended List
+print('Planet List: ', planet_list)
