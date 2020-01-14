fc228eefb192b04f27d4b5599b55bd2c6412f38e
Michelle Johnson <bob1234girl@gmail.com>
2020-01-13 21:18:07 -0600

    slice() and del

--- planets.py
+++ planets.py
@@ -1,13 +1,30 @@
 planet_list = ["Mercury", "Mars"]
 
+# Append list
 planet_list.append("Jupiter")
 planet_list.append("Saturn")
 print (planet_list)
 
-# another list of planets
-planet_list1 = ["Uranus", "Neptune"]
-
-planet_list.extend(planet_list1)
-
 # Extended List
+planet_list1 = ["Uranus", "Neptune"]
+planet_list.extend(planet_list1)
 print('Planet List: ', planet_list)
+
+# Insert List
+
+planet_list.insert(1, 'Earth')
+planet_list.insert(1, 'Venus')
+print('Updated List: ', planet_list)
+
+# Append List
+planet_list.append("Pluto")
+print ('Add Pluto',planet_list)
+
+# slice list
+planet_list2 = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
+rocky_planets = slice(1,4)
+print('Slice:', planet_list2[rocky_planets])
+
+# del pluto
+del planet_list[8]
+print('del', planet_list)
